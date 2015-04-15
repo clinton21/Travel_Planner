382c09a858222a0aa78c7523bd36dc12d33c82fb
Clinton D'souza <clinton@ajency.in>
2015-04-15 18:29:25 +0530

    no message

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,9 @@
  * Created by Clinton on 01-02-2015.
  */
 angular.module('mapsApp', ['ui.router', 'mapApp.controllers','mapApp.services'])
-    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-
+    .config(['$stateProvider', '$urlRouterProvider', '$httpProvider',function ($stateProvider, $urlRouterProvider,$httpProvider) {
+    	var contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+    	$httpProvider.defaults.headers.common['Content-Type'] = contentType;
         $stateProvider
             .state('viewMap', {
                 url: '/viewMap',
--- app/js/services.js
+++ app/js/services.js
@@ -1,8 +1,27 @@
-angular.module('mapApp.services',[])
+angular.module('mapApp.services', [])
 
-.service('userData',function($http){
+.service('userData', function($http, $q) {
 
-this.getUserData = function(){
-	return $http.get('assets/mock_user_data.json');
+	this.getUserData = function(circle_data) {
+		// http://awmphoenix.com/api/v1/phoenix-users/addresses?filters[firm_id]=205
+		var q = $q.defer();
+		$http.get("http://awmphoenix.com/api/v1/phoenix-users/addresses?"+$.param(circle_data))
+		.then(function(data){
+			console.log(data);
+			q.resolve(data);
+		},function(error){
+			console.log('Error fetching user Data');
+			q.reject('error');
+		});
+		return q.promise;
+	}
+	this.getEventData = function() {
+		var q = $q.defer();
+		$http.get('assets/mock_events.json').then(function(data) {
+			q.resolve(data);
+		},function(error){
+			console.log('Error fetching Event Data');
+		});
+		return q.promise;
 	}
 });
